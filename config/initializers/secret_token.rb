785dee968fcee20b8d2512f1a34d5cee4d69b8d9
chucai <hexudong08@gmail.com>
2012-06-01 15:34:59 +0800

    初始化项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photosgame::Application.config.secret_token = 'f0567e21fb8b4cf116ae35bbcda5eb1abfde557ca855e6169ac8bf16d1fae5b616581b1c36063e0f2984edf01fe6e623c95f81debc47628750af82dd72e3eaf4'
