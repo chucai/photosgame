fd5bc81190f57c28a1e1141afa3b18310ea47d40
chucai <hexudong08@gmail.com>
2012-06-05 19:01:59 +0800

    fix bugs

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,8 +3,12 @@ class PhotosController < ApplicationController
   def index
     token = params[:token]
     album = Album.find_by_token(token)
-    images = album.images
-    gon.images = images.map { |image|  image.file.url('main') }
+	if album
+      images = album.images
+      gon.images = images.map { |image|  image.file.url('main') }
+	else
+	  gon.images = []
+	end
   end
   
 end
